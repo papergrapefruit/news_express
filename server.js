a70203438ee935f58a85a6ea1f915ee9885a287b
papergrapefruit <statofeld@gmail.com>
2017-10-15 15:09:59 -0400

    committed

--- server.js
+++ server.js
@@ -39,7 +39,12 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://heroku_5t1jgflk:1a5k4m7sc3ar3oiko3evj0o3m7@ds013475.mlab.com:13475/heroku_5t1jgflk");
+
+var promise = mongoose.connect('mongodb://heroku_5t1jgflk:1a5k4m7sc3ar3oiko3evj0o3m7@ds013475.mlab.com:13475/heroku_5t1jgflk', {
+  useMongoClient: true,
+  /* other options */
+});
+// mongoose.connect("");mongodb://heroku_5t1jgflk:1a5k4m7sc3ar3oiko3evj0o3m7@ds013475.mlab.com:13475/heroku_5t1jgflk
 var db = mongoose.connection;
 
 // Show any mongoose errors
