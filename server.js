5f08653c13bfc9b47beea1c7de75999a1373f157
papergrapefruit <statofeld@gmail.com>
2017-10-15 14:56:51 -0400

    committed

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ app.post("/articles/:id", function (req, res) {
 });
 
 
-// Listen on port 3000
-app.listen(3000, function () {
-  console.log("App running on port 3000!");
-});
+// // Listen on port 3000
+// app.listen(3000, function () {
+//   console.log("App running on port 3000!");
+// });
